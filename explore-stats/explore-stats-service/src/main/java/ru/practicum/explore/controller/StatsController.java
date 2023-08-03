c74aab9b23380612d1e2f0132235cc5818128882
Egor Yakovlev <riddik24@yandex.ru>
2023-08-04 00:07:29 +0300

    refactor based on reviewers comments

--- explore-stats/explore-stats-service/src/main/java/ru/practicum/explore/controller/StatsController.java
+++ explore-stats/explore-stats-service/src/main/java/ru/practicum/explore/controller/StatsController.java
@@ -12,7 +12,6 @@ import ru.practicum.explore.service.StatsService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @Slf4j
@@ -42,21 +41,16 @@ public class StatsController {
                                                  @RequestParam(required = false) List<String> uris,
                                                  @RequestParam(required = false, defaultValue = "false") Boolean unique) {
 
-        try {
-            LocalDateTime startDate = LocalDateTime.parse(start, dateTimeFormatter);
-            LocalDateTime endDate = LocalDateTime.parse(end, dateTimeFormatter);
+        LocalDateTime startDate = LocalDateTime.parse(start, dateTimeFormatter);
+        LocalDateTime endDate = LocalDateTime.parse(end, dateTimeFormatter);
 
-            if (startDate.isAfter(endDate)) {
-                log.warn("Start date {} must be before end date {}.", startDate, endDate);
-                throw new IllegalArgumentException(String.format("Start date %s must be before end date %s.", startDate, endDate));
-            }
-            log.info("Getting hits from {} to {} for uris: {}. Unique: {}", startDate, endDate, uris, unique);
-            List<EndpointHitResponseDto> hits = statsService.getHits(startDate, endDate, uris, unique);
-            log.info("Hits: {}", hits);
-            return hits;
-        } catch (DateTimeParseException e) {
-            log.warn(e.getMessage());
-            throw e;
+        if (startDate.isAfter(endDate)) {
+            log.warn("Start date {} must be before end date {}.", startDate, endDate);
+            throw new IllegalArgumentException(String.format("Start date %s must be before end date %s.", startDate, endDate));
         }
+        log.info("Getting hits from {} to {} for uris: {}. Unique: {}", startDate, endDate, uris, unique);
+        List<EndpointHitResponseDto> hits = statsService.getHits(startDate, endDate, uris, unique);
+        log.info("Hits: {}", hits);
+        return hits;
     }
 }
